73807b5a3e859ccd13205afddf3dfabf86621ca9
robertli8629 <lycrt88@gmail.com>
2014-02-01 19:43:58 -0500

    updated

---
+++ a2/RUNTESTS.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to run all failing parser tests
+
+COMPILER=../../dist/compiler488.jar
+
+runtests () {
+for file in `ls ./*.488`
+do
+    echo "Running $file"
+    java -jar $COMPILER $file
+    echo
+done
+}
+
+pushd ./tests/failing > /dev/null
+runtests
+popd > /dev/null
+pushd ./tests/passing > /dev/null
+runtests
+popd > /dev/null
+
+echo "All tests run!"
+
+exit 0
