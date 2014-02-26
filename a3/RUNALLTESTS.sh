097977b59f870b7d34369c3c2a681c0c06243b3e
Geoff Haugan <geoff.haugan@mail.utoronto.ca>
2014-02-26 17:09:58 -0500

    Adding A2 test script to A3.

---
+++ a3/RUNALLTESTS.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script to run all parser tests
+
+WHERE=.
+COMPILER=../../dist/compiler488.jar
+
+runtests () {
+for file in `ls ./*.488`
+do
+    echo "Running $file"
+    java -jar $COMPILER $file
+    echo
+done
+}
+
+pushd $WHERE/tests/failing > /dev/null
+runtests
+popd > /dev/null
+pushd $WHERE/tests/passing > /dev/null
+runtests
+popd > /dev/null
+
+echo "All tests run!"
+
+exit 0
